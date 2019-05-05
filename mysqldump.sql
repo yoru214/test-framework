9d440d229a0dd1800bcfe738f69085df645f0d29
Emmanuel Zerna <emzer214@gmail.com>
2019-05-05 18:33:29 +0800

    Added MySQL Dump file of the working database.

---
+++ mysqldump.sql
@@ -0,0 +1,122 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.25 : Database - testdb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`testdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `testdb`;
+
+/*Table structure for table `carts` */
+
+DROP TABLE IF EXISTS `carts`;
+
+CREATE TABLE `carts` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(10) DEFAULT NULL,
+  `product_id` int(10) DEFAULT NULL,
+  `quantity` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `carts` */
+
+/*Table structure for table `customers` */
+
+DROP TABLE IF EXISTS `customers`;
+
+CREATE TABLE `customers` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `image_url` text,
+  `funds` decimal(10,2) DEFAULT '100.00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `customers` */
+
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `description` text,
+  `image` text,
+  `price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `products` */
+
+insert  into `products`(`id`,`name`,`description`,`image`,`price`) values (1,'Apple','\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"','https://images-na.ssl-images-amazon.com/images/I/81xQBb5jRzL._SY355_.jpg','0.30');
+insert  into `products`(`id`,`name`,`description`,`image`,`price`) values (2,'Beer','\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"','https://target.scene7.com/is/image/Target/GUEST_48ef432f-09f1-47ac-9b07-99a5540a908e?wid=488&hei=488&fmt=pjpeg','2.00');
+insert  into `products`(`id`,`name`,`description`,`image`,`price`) values (3,'Water','\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"','https://target.scene7.com/is/image/Target/GUEST_7fc0cb08-5552-41ce-94c7-ab075a048a03?wid=488&hei=488&fmt=pjpeg','1.00');
+insert  into `products`(`id`,`name`,`description`,`image`,`price`) values (4,'Cheese','\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"','https://az836796.vo.msecnd.net/media/image/product/en/medium/0006810000580.jpg','3.74');
+
+/*Table structure for table `purchases` */
+
+DROP TABLE IF EXISTS `purchases`;
+
+CREATE TABLE `purchases` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(10) DEFAULT NULL,
+  `product_id` int(10) DEFAULT NULL,
+  `quantity` int(10) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `shipping` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `purchases` */
+
+/*Table structure for table `ratings` */
+
+DROP TABLE IF EXISTS `ratings`;
+
+CREATE TABLE `ratings` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `rate` tinyint(2) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `ratedatetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `ratings` */
+
+/*Table structure for table `shippings` */
+
+DROP TABLE IF EXISTS `shippings`;
+
+CREATE TABLE `shippings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `component_class` varchar(100) DEFAULT NULL,
+  `fee` decimal(10,2) DEFAULT '0.00',
+  `fee_type` enum('FLAT','PERCENT') DEFAULT 'FLAT',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE_NAME` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `shippings` */
+
+insert  into `shippings`(`id`,`name`,`description`,`component_class`,`fee`,`fee_type`,`active`) values (1,'pickup','Pick Up','StandardShipping','0.00','FLAT',1);
+insert  into `shippings`(`id`,`name`,`description`,`component_class`,`fee`,`fee_type`,`active`) values (2,'ups','UPS','UPSShipping','5.00','FLAT',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
